36da9ec8c354a39f3a73546b555e983af443c7ff
AdityaBheke <adityabheke34@gmail.com>
2024-07-11 20:48:27 +0530

    Set up default route and error msg for invalid url

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+// Setup
+const server = express();
+server.use(bodyParser.json());
+
+// Routes
+
+server.get('/',(req,res)=>{
+    res.send('Welcome to Postaway!');
+})
+server.use((req,res)=>{
+    res.status(404).send('API not found.')
+})
+
+// Listen
+server.listen(3100,()=>{
+    console.log('Server is listening on port 3100');
+})
