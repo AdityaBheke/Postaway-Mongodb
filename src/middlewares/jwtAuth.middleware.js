1b6cc3c7c43aa483374aca5b8e5bc635ed2335cc
AdityaBheke <adityabheke34@gmail.com>
2024-07-14 05:20:39 +0530

    access jwt token  via authorization header instead of cookie

--- src/middlewares/jwtAuth.middleware.js
+++ src/middlewares/jwtAuth.middleware.js
@@ -2,16 +2,25 @@ import jwt from "jsonwebtoken";
 import { customError } from "../errorHandler/errorHandler.middleware.js";
 
 const jwtAuth = (req, res, next)=>{
-    const cookie = req.headers.cookie;
-    if (!cookie) {
-        throw new customError(400, 'Bad request Token not available');
+    // Passing token via authorization header
+    const token = req.headers['authorization'];
+    if (!token) {
+        throw new customError(401, 'Bad request Token not available');
     }
-    const token = cookie.replace('jwtToken=','');
+
+    // Passing token via cookie
+    
+    // const cookie = req.headers.cookie;
+    // if (!cookie) {
+    //     throw new customError(401, 'Bad request Token not available');
+    // }
+    // const token = cookie.replace('jwtToken=','');
+
     try {
         const payload = jwt.verify(token,'EpGbiLNDm5');
         req.userId = payload.userId;
     } catch (error) {
-        throw new customError(400, error);
+        throw new customError(401, "Token expired");
     }
     next();
 }
